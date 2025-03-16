3a80cadc409a917004953bc0e1c29d13b7bbd8ba
dijolev <diegovallejocei@gmail.com>
2025-03-15 19:27:41 -0500

    Agregar archivos de proyecto.

---
+++ Models/Entidades/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entidades
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime? DueDate { get; set; } // ¿Está incluido?
+        public bool Completed { get; set; }
+    }
+}
